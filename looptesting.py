2c95943fc948e6ad07ac5ce610256d65594a54d5
cs-c92 <81988507+cs-c92@users.noreply.github.com>
2021-07-26 21:10:37 +0100

    comments
    added comments

--- looptesting.py
+++ looptesting.py
@@ -1,3 +1,4 @@
+# for loops
 for number in range(3):
     print("Test", number + 1)
 
@@ -5,18 +6,20 @@ summon = "Beetlejuice!"
 for s in range(3):
     print(summon)
 
-
+# while loop
 countdown = 5
 while countdown >= 1:
     print(countdown, (countdown - 1) * "."); countdown -= 1
 print("Thunderbirds are go!")
 
+# for list
 print("You must not feed a bunny:")
 do_not_feed_bunny = ["Tomato greens", "Chocolate", "Avocado", "Nuts", "Potatoes", "Meat"]
 for food in do_not_feed_bunny:
     print(food)
 print("Or anything else they try to steal")
 
+# while list
 feed_the_bunny = ["Hay", "Kale", "Nuggets", "Herbs"]
 length = len(feed_the_bunny)
 index = 0
