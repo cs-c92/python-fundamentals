db05457c901af6748e8d24fa1d3c64337f877120
cs-c92 <81988507+cs-c92@users.noreply.github.com>
2021-07-26 20:19:13 +0100

    Countdown dots
    Counting down in dots

--- looptesting.py
+++ looptesting.py
@@ -8,5 +8,6 @@ for s in range(3):
 
 countdown = 5
 while countdown >= 1:
-    print(countdown); countdown -= 1
+    print(countdown, (countdown - 1) * "."); countdown -= 1
 print("Thunderbirds are go!")
+
