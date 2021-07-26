7760b1c2174895056476033a792429df1d9670f7
cs-c92 <81988507+cs-c92@users.noreply.github.com>
2021-07-26 21:02:20 +0100

    don't feed
    for loop, with a list

--- looptesting.py
+++ looptesting.py
@@ -11,6 +11,11 @@ while countdown >= 1:
     print(countdown, (countdown - 1) * "."); countdown -= 1
 print("Thunderbirds are go!")
 
+print("You must not feed a bunny:")
+do_not_feed_bunny = ["Tomato greens", "Chocolate", "Avocado", "Nuts", "Potatoes", "Meat"]
+for food in do_not_feed_bunny:
+    print(food)
+print("Or anything else they try to steal")
 
 feed_the_bunny = ["Hay", "Kale", "Nuggets", "Herbs"]
 length = len(feed_the_bunny)
