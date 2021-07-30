7d78acc69040ddb9d4e001e63d84052b1d1a26d1
cs-c92 <81988507+cs-c92@users.noreply.github.com>
2021-07-30 19:29:24 +0100

    break
    using a break

---
+++ loop_breaks.py
@@ -0,0 +1,8 @@
+herbs_for_bunny = ["cilantro", "mint", "basil", "parsley", "sage", "rosemary"]
+bunnys_favourite = "basil"
+
+for herb in herbs_for_bunny:
+    print(herb)
+    if herb == bunnys_favourite:
+        print("That's his favourite!")
+        break
