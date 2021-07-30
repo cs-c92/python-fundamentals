78dada5b81908f3cbf631a287881981d44593d32
cs-c92 <81988507+cs-c92@users.noreply.github.com>
2021-07-30 19:36:53 +0100

    continue
    continue list

--- loop_breaks.py
+++ loop_breaks.py
@@ -6,3 +6,10 @@ for herb in herbs_for_bunny:
     if herb == bunnys_favourite:
         print("That's his favourite!")
         break
+
+num_list = [35, 62, 73, 42, 17, 19, 6, 41, 37, 24]
+
+for num in num_list:
+    if num > 31:
+        continue
+    print(num)
