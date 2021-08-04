25e794a0cc3bd2ce8044cf6255eea7b95077d6cf
cs-c92 <81988507+cs-c92@users.noreply.github.com>
2021-08-04 16:52:29 +0100

    checking length

--- more_lists.py
+++ more_lists.py
@@ -7,3 +7,11 @@ def append_sum(nums):
 append_sum([1, 2, 3, 4, 5])
 print(append_sum([1, 2, 3, 4, 5]))
 
+# ifs and lists
+def two_lists(one, two):
+    if len(one) >= len(two):
+        return one[-1]
+    else:
+        return two[-1]
+two_lists([2, 4, 6, 8, 10], [1, 3, 4, 7, 9])
+print(two_lists([2, 4, 6, 8, 10], [1, 3, 4, 7, 9]))
